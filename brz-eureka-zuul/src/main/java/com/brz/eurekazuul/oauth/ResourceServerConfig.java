6505b751f2111dd3c44d1a13ef4d8c6db0ab51e0
Richard Daniel Oliva <danielolivadenis@gmail.com>
2020-01-24 20:39:36 +0100

    Zuul cors update

--- brz-eureka-zuul/src/main/java/com/brz/eurekazuul/oauth/ResourceServerConfig.java
+++ brz-eureka-zuul/src/main/java/com/brz/eurekazuul/oauth/ResourceServerConfig.java
@@ -37,7 +37,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers("/api/security/oauth/**").permitAll()
+			.antMatchers("/api/**").permitAll()
+//			.antMatchers("/api/security/oauth/**").permitAll()
 //		.antMatchers(HttpMethod.GET, "/api/productos/listar", "/api/items/listar", "/api/usuarios/usuarios").permitAll()
 //		.antMatchers(HttpMethod.GET, "/api/productos/ver/{id}", 
 //				"/api/items/ver/{id}/cantidad/{cantidad}", 
