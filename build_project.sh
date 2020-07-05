3c6f31b0f0e75f2dc2b87e65e66048c09d0b6b35
RichardDanielOliva <45484309+RichardDanielOliva@users.noreply.github.com>
2020-07-05 15:29:46 +0200

    Update build_project

--- build_project.sh
+++ build_project.sh
@@ -9,11 +9,6 @@ docker-compose up --build config-server
 echo "----- DESPLEGANDO CONTENEDOR DOCKER:    eureka-server      -----"
 docker-compose up --build eureka-server
 
-until curl -f http://localhost:8761; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
-
 #docker-compose up --build eureka-zuul
 
 #docker-compose up --build
