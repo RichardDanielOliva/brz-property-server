44e18136ab4d5c582f3a2fcf4e0deb289ef66378
RichardDanielOliva <45484309+RichardDanielOliva@users.noreply.github.com>
2020-07-10 13:00:07 +0200

    Update build_test sh

---
+++ build_project_test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#echo "----- CONSTRUYENDO .JAR CON MAVEN -----"
+mvn install -DskipTests
+echo "----- GENERANDO Y DESPLEGANDO CONTENEDORES DOCKER -----"
+
+echo "----- DESPLEGANDO CONTENEDOR DOCKER:    config-server      -----"
+docker-compose up -d config-server
+
+echo "----- DESPLEGANDO CONTENEDOR DOCKER:    eureka-server      -----"
+docker-compose up -d eureka-server
+
+until curl -f "http://localhost:8761"; do
+  >&2 echo "eureka-server no se ha desplegado todavia"
+  sleep 10
+done
+
+echo "----- DESPLEGANDO CONTENEDORES DOCKER ms 1er nivel models  -----"
+docker-compose up -d ms-model-home ms-model-user
+
+echo "----- DESPLEGANDO CONTENEDOR DEL API GATEWAY: eureka-zuul"
+docker-compose up -d eureka-zuul
+
+echo "----- DESPLEGANDO CONTENEDORES  PARA SEGURIDAD: server-authentification"
+docker-compose up -d server-authentification
+
+echo "----- DESPLEGANDO CONTENEDORES  PARA SEGURIDAD: security-oauth"
+docker-compose up -d security-oauth
+
+
+
+
