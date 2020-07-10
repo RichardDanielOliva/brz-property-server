ef1910d9a7a27756ec9f911470d725c2ca1964a1
RichardDanielOliva <45484309+RichardDanielOliva@users.noreply.github.com>
2020-07-10 22:10:29 +0200

    Update build_project

--- build_project_test.sh
+++ build_project_test.sh
@@ -14,11 +14,12 @@ until curl -f "http://localhost:8761"; do
   sleep 10
 done
 
-echo "----- DESPLEGANDO CONTENEDORES DOCKER ms 1er nivel models  -----"
-docker-compose up -d ms-model-home ms-model-user
-
 echo "----- DESPLEGANDO CONTENEDOR DEL API GATEWAY: eureka-zuul"
 docker-compose up -d eureka-zuul
+
+echo "----- DESPLEGANDO CONTENEDORES DOCKER ms 1er nivel models  -----"
+docker-compose up -d ms-model-home ms-model-office ms-model-premise ms-model-user
+
 
 echo "----- DESPLEGANDO CONTENEDORES  PARA SEGURIDAD: server-authentification"
 docker-compose up -d server-authentification
@@ -26,6 +27,5 @@ docker-compose up -d server-authentification
 echo "----- DESPLEGANDO CONTENEDORES  PARA SEGURIDAD: security-oauth"
 docker-compose up -d security-oauth
 
-
-
-
+echo "----- TODOS LOS CONTENEDORES HAN SIDO INICIADO      -----"
+sleep 30
